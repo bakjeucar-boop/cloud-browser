069198c46a28e5af3442f0bedad3bf9639f38124
bakjeucar-boop <bakjeucar@gmail.com>
2026-08-14 10:47:26 +0900

    Replace Firefox with Dillo and add clipboard sync

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,13 +7,16 @@ sleep 2
 # 2. 윈도우 매니저 실행
 env DISPLAY=:0 fluxbox &
 
-# 3. VNC 서버 실행
+# 3. 클립보드 동기화 데몬 실행
+autocutsel -fork &
+autocutsel -selection PRIMARY -fork &
+
+# 4. VNC 서버 실행
 x11vnc -display :0 -nopw -listen localhost -xkb -forever -shared -bg
 
-# 4. 웹 브라우저(Firefox ESR) 실행
-# 쓰기 권한 문제가 발생하지 않도록 임시 프로필 디렉터리를 생성하여 지정합니다.
-mkdir -p /tmp/ff_profile
-env DISPLAY=:0 firefox --profile /tmp/ff_profile --no-sandbox --window-size=1024,768 &
+# 5. 극경량 브라우저(dillo) 및 터미널(xterm) 실행
+env DISPLAY=:0 xterm -geometry 80x24+10+10 &
+env DISPLAY=:0 dillo &
 
-# 5. 웹 소켓 프록시(websockify) 실행
+# 6. 웹 소켓 프록시(websockify) 실행
 websockify --web /usr/share/novnc 8080 localhost:5900
